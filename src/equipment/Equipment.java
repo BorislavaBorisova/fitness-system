5ef02db8c5e65f56a1d1233d54ce4193f8b6b5cc
Borislava Borisova <bori.plamenowa@gmail.com>
2018-10-27 17:13:04 +0300

    Equipments is added only by the administrator.

---
+++ src/equipment/Equipment.java
@@ -0,0 +1,27 @@
+package equipment;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Equipment implements Serializable{
+    Date dateLastAdded;
+    String typeOfEquipment;
+    int count;
+
+    public Equipment(String setTypeOfEquipment) {
+        dateLastAdded = Calendar.getInstance().getTime();
+        typeOfEquipment = setTypeOfEquipment;
+        count = 1;
+    }
+
+    public void addExistingEquipment() {
+        count++;
+        dateLastAdded = Calendar.getInstance().getTime();
+    }
+
+    public Date getDate() {
+        return dateLastAdded;
+    }
+
+}
